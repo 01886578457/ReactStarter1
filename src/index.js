757c08fbea62a0762c57cb42733a5887befc5a74
DESKTOP-TOOTMLU\DuyLongPc <LongTMGCS16297@fpt.edu.vn>
2018-12-29 18:27:27 +0700

    state management

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return <div>
-        Hi
-    </div>
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { lat: null, long: null, errMsg: '' };
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude, long: position.coords.longitude })
+            },
+            (err) => {
+                this.setState({ errMsg: err.message });
+            }
+        );
+    }
+
+    render() {
+        const { state } = this;
+        if (state.errMsg && !this.state.lat && !this.state.long) {
+            return (
+                <div>Err: {state.errMsg}</div>
+            )
+        } else {
+            return (
+                <div>
+                    Lat: {state.lat} ,
+                    Long: {state.long}
+                </div>
+            )
+        }
+    }
 }
 
 ReactDOM.render(
-    <App/>,
+    <App />,
     document.querySelector('#root')
 )
